d697748916e2e4ea7ac94a1ee84d10a91a6c2f9c
Julie Moura <18290320701@cefet-rj.br>
2026-05-28 23:07:31 -0300

    feat(routes): refactor AppRoutes and ProtectedRoute for improved authentication handling

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,32 @@
 import { Routes, Route } from "react-router";
 
-import Login from "../pages/Login";
-
+import Login from "@/pages/Login";
 import ProtectedRoute from "./ProtectedRoute";
-import Dashboard from "../pages/Dashboard";
+import Dashboard from "@/pages/Dashboard";
+import Entries from "@/pages/Entries";
+import { store } from "@/store/store";
+import Layout from "@/pages/Layout";
+import { Provider } from "react-redux";
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Login />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="entries" element={<Entries />} />
+        </Route>
+      </Routes>
+    </Provider>
   );
 }
 
--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router";
+import { useAppSelector } from "@/store/hooks";
 
 type Props = {
   children: React.ReactNode;
 };
 
 function ProtectedRoute({ children }: Props) {
-  const isAuthenticated = true;
+  const isAuthenticated = useAppSelector((state) => !!state.auth.token); // usando o token do redux para verificar a autenticação, e ai caso não tenha token, o usuário é redirecionado para a página de login
 
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
