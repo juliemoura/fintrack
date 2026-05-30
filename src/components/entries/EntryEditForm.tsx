99f456d2b949664c66edff41b0e4ca96360fa174
Julie Moura <18290320701@cefet-rj.br>
2026-05-30 13:03:47 -0300

    feat(EntryEditForm): implement entry editing form with validation and toast notifications

---
+++ src/components/entries/EntryEditForm.tsx
@@ -0,0 +1,128 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/Dialog";
+import { Button } from "../ui/Button";
+import { Pencil } from "lucide-react";
+import EntryFormFields from "./EntryFormFields";
+import { useAppDispatch } from "@/store/hooks";
+import { useForm } from "react-hook-form";
+import { updateEntry, type Entry } from "@/store/entrySlice";
+import { entrySchema, type EntrySchema } from "./schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { Toast } from "../ui/Toast";
+
+interface EntryEditFormProps {
+  entry: Entry;
+}
+
+const EntryEditForm = ({ entry }: EntryEditFormProps) => {
+  const dispatch = useAppDispatch(); // dispatch para enviar a nova entrada para o store
+
+  const [open, setOpen] = useState(false);
+  const [toast, setToast] = useState<string | null>(null);
+
+  const {
+    control,
+    setValue,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<EntrySchema>({
+    resolver: zodResolver(entrySchema),
+    defaultValues: {
+      type: entry.type,
+      category: entry.category,
+      value: entry.value,
+      date: entry.date,
+      description: entry.description,
+    },
+  });
+
+  useEffect(() => {
+    reset({
+      type: entry.type,
+      category: entry.category,
+      value: entry.value,
+      date: entry.date,
+      description: entry.description,
+    });
+  }, [entry, reset]);
+
+  const onSubmit = (data: EntrySchema) => {
+    dispatch(
+      updateEntry({
+        id: entry.id,
+        type: (data.type ?? "receita") as "receita" | "despesa",
+        category: (data.category ?? "outros") as
+          | "poupanca"
+          | "moradia"
+          | "alimentacao"
+          | "transporte"
+          | "lazer"
+          | "saude"
+          | "outros",
+        value: data.value ?? 0,
+        date: data.date ?? "",
+        description: data.description ?? "",
+      }),
+    );
+    reset();
+    setOpen(false);
+    setToast("Entrada editada com sucesso!");
+  };
+
+  if (!entry) return null; // Se a entrada não existir, não renderiza o formulário
+
+  return (
+    <>
+      {toast && <Toast message={toast} onClose={() => setToast(null)} />}
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="bg-green-700">
+            <Pencil />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Formulário de Entrada</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>
+            Preencha os campos para editar a entrada.
+          </DialogDescription>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full flex flex-col gap-2"
+          >
+            <EntryFormFields
+              control={control}
+              setValue={setValue}
+              errors={errors}
+            />
+
+            <div className="flex gap-2 justify-end">
+              <Button
+                className="bg-red-700 w-max"
+                type="button"
+                onClick={() => setOpen(false)}
+              >
+                Cancelar
+              </Button>
+              <Button className="bg-green-700 w-max" type="submit">
+                Editar
+              </Button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default EntryEditForm;
