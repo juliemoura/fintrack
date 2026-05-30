e0af948bda4eaa6ff1eb2fa449bbf4a10f3a3e7f
Julie Moura <18290320701@cefet-rj.br>
2026-05-30 13:03:27 -0300

    feat(LoginForm): implement login form with validation, error handling, and remember me option

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,99 @@
+import { KeyRound, Mail } from "lucide-react";
+import { Button } from "@/components/ui/Button/Button";
+import { Input } from "@/components/ui/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { loginUser, clearError } from "@/store/authSlice";
+
+import { loginSchema, type LoginSchema } from "./schema";
+import { useForm } from "react-hook-form";
+import { Toast } from "@/components/ui/Toast";
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+const LoginForm = () => {
+  const [toastError, setToastError] = useState<string | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const { status } = useAppSelector((state) => state.auth);
+  const isSubmitting = status === "loading";
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  async function onSubmit(data: LoginSchema) {
+    dispatch(clearError());
+
+    const result = await dispatch(loginUser(data));
+
+    if (loginUser.fulfilled.match(result)) {
+      navigate("/dashboard");
+    }
+
+    if (loginUser.rejected.match(result)) {
+      setToastError(result.payload as string);
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex flex-col gap-5"
+    >
+      <Input
+        label="E-mail"
+        icon={<Mail size={18} />}
+        placeholder="seu@email.com"
+        {...register("email")}
+        type="email"
+        error={errors.email?.message}
+      />
+
+      <Input
+        label="Senha"
+        icon={<KeyRound size={18} />}
+        placeholder="Sua senha"
+        type="password"
+        {...register("password")}
+        error={errors.password?.message}
+      />
+
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Input
+            type="checkbox"
+            className="rounded border-2 border-slate-600 bg-slate-800 checked:bg-[#6366f1] accent-[#6366f1] m-0 p-0"
+          />
+          <span className="text-sm text-white">Lembrar de mim</span>
+        </div>
+        <a href="#" className="text-sm text-[#6366f1] hover:text-[#818cf8]">
+          Esqueci minha senha?
+        </a>
+      </div>
+
+      {toastError && (
+        <Toast message={toastError} onClose={() => setToastError(null)} />
+      )}
+
+      <Button
+        type="submit"
+        disabled={isSubmitting}
+        className="w-full bg-[#6366f1] text-white py-3 rounded-md font-semibold mt-4 hover:from-[#5558e3] hover:to-[#3d38d9] transition-all shadow-lg hover:scale-[1.02]"
+      >
+        {isSubmitting ? "Entrando..." : "Entrar"}
+      </Button>
+    </form>
+  );
+};
+
+export default LoginForm;
