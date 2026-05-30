7f000a3224683dfcfa21447b9737e581486068ad
Julie Moura <18290320701@cefet-rj.br>
2026-05-30 13:04:41 -0300

    feat(Login): update Login component structure and styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import { Wallet } from "lucide-react";
-import LoginForm from "../components/auth/LoginForm";
-import heroIllustration from "../assets/fintrack_hero_illustration.svg";
+import LoginForm from "@/components/login/LoginForm";
+import heroIllustration from "@/assets/fintrack_hero_illustration.svg";
 
 function Login() {
   return (
-    <div className="min-h-screen bg-background text-text w-full lg:grid lg:grid-cols-2">
+    <div className="min-h-screen text-text w-full lg:grid lg:grid-cols-2">
       <div
         className="hidden lg:flex flex-col items-center justify-center p-10 bg-cover bg-center"
         style={{
@@ -15,19 +15,18 @@ function Login() {
       />
 
       <div className="flex flex-col items-center justify-center p-6 sm:p-10">
-        <div className="flex flex-col gap-2 items-center mb-12">
-          <Wallet size={40} className="text-primary" />
-          <h1 className="font-heading text-3xl">
-            Fin<span className="text-primary-light">Track</span>
+        <div className="flex flex-col gap-2 items-center mb-16">
+          <Wallet size={40} className="text-[#6366f1]" />
+          <h1 className="font-heading text-4xl font-bold">
+            Fin
+            <span className="text-[#6366f1]">Track</span>
           </h1>
         </div>
 
         <div className="w-full max-w-md">
-          <div className="mb-8 text-center">
-            <h2 className="font-heading text-3xl font-semibold">
-              Bem-vindo de volta!
-            </h2>
-            <h3 className="text-text-muted mt-2">
+          <div className="mb-10 text-center">
+            <h3 className="text-white text-2xl">Bem vindo de volta!</h3>
+            <h3 className="text-slate-400 text-sm">
               Faça login para acessar sua conta.
             </h3>
           </div>
